b178629c5c5f3805f15f2dab1a972e6a9c296d2f
SergioArgente <141647302+SergioArgente@users.noreply.github.com>
2025-11-11 03:53:46 +0100

    Update libreria.js

--- codigo/libreria.js
+++ codigo/libreria.js
@@ -1,7 +1,7 @@
 //Funciones reutilizables
 
 
-// Función para verificar siExiste una propiedad de un array____________________________________________
+// ------------------- SIEXISTE PROPIEDAD --------------------
 function existeProp(array, propiedad, valor)
 {
     for (let i = 0; i < array.length; i++)
@@ -14,8 +14,9 @@ function existeProp(array, propiedad, valor)
     return false;
 }
 
-// Función para verificar contraseña valida__________________________________________________________
-function validaPass(contrasena)
+// ------------------- VALIDAR CONTRASEÑA --------------------
+
+function validarContrasena (contrasena)
 {
     if (contrasena.length < 6)
     {
@@ -62,7 +63,61 @@ function validaPass(contrasena)
         return false;
     }
     
-} //Fin validaPass
+}
+
+// ------------------- GENERAR ID'S --------------------
+//El id debe ser único, por lo que se requiere un formato de id, diferente para cada clase
+//Seguimos el mismo criterio que el declarado en la letra: CON_ID_ para conciertos.
+
+let proximoIdConcierto = 1;
+let proximoIdCliente = 1;
+let proximoIdAdministrador = 1;
+let proximoIdReserva = 1;
+
+function obtenerIdConcierto()
+{
+    let idActual = "CON_ID_" + proximoIdConcierto; 
+    proximoIdConcierto++;
+    return idActual;
+}
+
+function obtenerIdCliente()
+{
+    let idCliente = "CLI_ID_" + proximoIdCliente;
+    proximoIdCliente++;
+    return idCliente;
+}
+
+function obtenerIdAdministrador()
+{
+    let idActual = "ADM_ID_" + proximoIdAdministrador;
+    proximoIdAdministrador++;
+    return idActual;
+}
+
+function obtenerIdReserva()
+{
+    let idActual = "RES_ID_" + proximoIdReserva;
+    proximoIdReserva++;
+    return idActual;
+}
+
+// ------------------- OBTENER STRING DE BOOLEANO --------------------
+// Creamos esta función para poder obtener el texto ("Si, No"), del parámetro 'Oferta'.
+// Y así poder llevarlo a tablas dinámicas de los conciertos.
+
+
+function obtenerTexto(valor)
+{
+    if (valor === true)
+    {
+        return "Si";
+    }
+    else
+    {
+        return "No";
+    }
+}
 
 // ------------------- VALIDACIONES RESERVAR --------------------
 
