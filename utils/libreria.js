7edcfc7d2ce942f8a411e99154f1c7ac1ce18f60
Yarisdalia <yarisdaliahinojosapadilla2001@gmail.com>
2025-11-12 18:06:06 -0300

    refactor

--- utils/libreria.js
+++ utils/libreria.js
@@ -1,5 +1,56 @@
 //Funciones reutilizables
 
+// ------------------- TIPO DE USUARIO --------------------
+
+function esAdmin(user) {
+  return user && user instanceof Administrador;
+}
+
+function esCliente(user) {
+  return user && user instanceof Cliente;
+}
+
+// ------------------- VALIDAR CONTRASEÑA --------------------
+
+function validarContrasena(contrasena) {
+  if (contrasena.length < 5) {
+    return false;
+  }
+
+  let tieneMayuscula = false;
+  let tieneMinuscula = false;
+  let tieneNumero = false;
+  let todasValidas = false;
+
+  for (let i = 0; i < contrasena.length; i++) {
+    let caracter = contrasena.charAt(i);
+
+    if (caracter >= "A" && caracter <= "Z") {
+      tieneMayuscula = true;
+    }
+
+    if (caracter >= "a" && caracter <= "z") {
+      tieneMinuscula = true;
+    }
+
+    if (caracter >= "0" && caracter <= "9") {
+      tieneNumero = true;
+    }
+
+    if (tieneMayuscula && tieneMinuscula && tieneNumero) {
+      todasValidas = true;
+      break;
+    }
+  }
+
+  if (todasValidas) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 // ------------------- SIEXISTE PROPIEDAD --------------------
 function existeProp(array, propiedad, valor) {
   for (let i = 0; i < array.length; i++) {
--- utils/tipo-de-usuario.js
+++
@@ -1,7 +0,0 @@
-function esAdmin(user) {
-  return user && user instanceof Administrador;
-}
-
-function esCliente(user) {
-  return user && user instanceof Cliente;
-}
